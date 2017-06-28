e14cda400e6a57c9f29e7241e46d396d6039db67
Pilgrimage <nanobot149@gmail.com>
2017-06-28 18:09:21 +0300

    continue...

---
+++ Ch01_DefiningClasses/p06_CompanyRoster/CompanyRoster.cs
@@ -0,0 +1,73 @@
+﻿using System.ComponentModel;
+using System.Linq;
+
+namespace p06_CompanyRoster
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class CompanyRoster
+    {
+        public static void Main()
+        {
+            List<Employee> personal = new List<Employee>();
+            int count = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < count; i++)
+            {
+                string[] input = Console.ReadLine().Split(new char[] {' '}, StringSplitOptions.RemoveEmptyEntries);
+                string name = input[0];
+                decimal salary = decimal.Parse(input[1]);
+                string position = input[2];
+                string department = input[3];
+                string email;
+                int age;
+                switch (input.Length)
+                {
+                    case 4:
+                        personal.Add(new Employee(name, salary, position, department));
+                        break;
+
+                    case 5:
+                        if (int.TryParse(input[4], out age))
+                        {
+                            personal.Add(new Employee(name, salary, position, department, age));
+                        }
+                        else
+                        {
+                            email = input[4];
+                            personal.Add(new Employee(name, salary, position, department, email));
+                        }
+                        break;
+
+                    case 6:
+                        email = input[4];
+                        age = int.Parse(input[5]);
+                        personal.Add(new Employee(name, salary, position, department, email, age));
+                        break;
+                }
+            }
+
+            var selected = personal
+                .GroupBy(gr => gr.Department)
+                .Select(x => new
+                {
+                    Department = x.Key,
+                    AvgSalary = x.Average(s => s.Salary),
+                    Employees = x.OrderByDescending(s => s.Salary)
+                })
+                .OrderByDescending(s => s.AvgSalary)
+                .FirstOrDefault();
+
+            
+            Console.WriteLine($"Highest Average Salary: {selected.Department}");
+
+
+            foreach (Employee emp in selected.Employees)
+            {
+                Console.WriteLine($"{emp.Name} {emp.Salary:f2} {emp.Email} {emp.Age}");
+            }
+
+        }
+    }
+}
---
+++ Ch01_DefiningClasses/p06_CompanyRoster/Employee.cs
@@ -0,0 +1,67 @@
+﻿namespace p06_CompanyRoster
+{
+    public class Employee
+    {
+        private string name;
+        private decimal salary;
+        private string position;
+        private string department;
+        private string email;
+        private int age;
+
+        public string Name
+        {
+            get { return this.name; }
+            set { this.name = value; }
+        }
+        public decimal Salary
+        {
+            get { return this.salary; }
+            set { this.salary = value; }
+        }
+        public string Position
+        {
+            get { return this.position; }
+            set { this.position = value; }
+        }
+        public string Department
+        {
+            get { return this.department; }
+            set { this.department = value; }
+        }
+        public string Email
+        {
+            get { return this.email; }
+            set { this.email = value; }
+        }
+        public int Age
+        {
+            get { return this.age; }
+            set { this.age = value; }
+        }
+
+
+        public Employee(string name, decimal salary, string position, string department) : this(name, salary, position, department, "n/a", -1)
+        {
+        }
+
+        public Employee(string name, decimal salary, string position, string department, string email) : this(name, salary, position, department, email, -1)
+        {
+        }
+
+        public Employee(string name, decimal salary, string position, string department, int age) : this(name, salary, position, department, "n/a", age)
+        {
+        }
+
+        public Employee(string name, decimal salary, string position, string department, string email, int age)
+        {
+            this.Name = name;
+            this.Salary = salary;
+            this.Position = position;
+            this.Department = department;
+            this.Email = email;
+            this.Age = age;
+        }
+
+    }
+}
